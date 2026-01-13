230d34796cf9dc1b5ce7acab827185e6df4f7c3d
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-01-13 11:28:30 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,5 @@
+a=("dilip", "kumar","hero")
+#print(type(a))
+#a.append("game")/statement is incorrect because Tuple is immutable
+#a.remove("hero")/same here
+print(a)
---
+++ pythonlist.py
@@ -0,0 +1,13 @@
+a= ["Dilip", "Govind", "Manoj"]
+#print(a)
+#print(type(a))
+a.append("Ajay")
+print(a)
+for x in a:
+    if x== 'kumar':
+        print(True)
+        break
+    else:
+        print(False)
+        break
+
