432444873591ff0d106aaabaa78875362c99cdef
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-01-16 11:18:21 +0530

    Data_types

---
+++ Data_type.py
@@ -0,0 +1,13 @@
+a="dilip"
+b=1234
+c='D'
+d=45.6
+print (type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Python is dynamically typed, so the data type is decided automatically
+# based on the value assigned. Common data types include numeric,
+# string, boolean, collection types, and None.
+#so ,therefore not declaration is required for datatypes in python.
