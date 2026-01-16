2d2ae2db4c51f5b9078e1da0b5550a932c55c9f0
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-01-16 11:40:47 +0530

    loops

---
+++ python_loop.py
@@ -0,0 +1,16 @@
+#WHILE_LOOP
+a="dilip"
+i=1
+while i<7:
+ print(a)
+ i=i+1
+
+#print("done")
+# FOR_LOOP
+b=[123,456,789,109]
+for x in b:
+    print(x)
+# some more about loop
+# start ,stop, step
+for y in range(1,30,2):# here 1 is start for loop ,30 is loop end, and 2 is step for the loop
+    print(y)
