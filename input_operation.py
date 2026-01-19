f7da31902d6e108400e759aa85a4ac0622fce517
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-01-19 11:05:09 +0530

    input_opreation with if_else/elif statement

---
+++ input_operation.py
@@ -0,0 +1,14 @@
+#input operation is important in python for taking user data for multiple operation
+#string data
+a=input("enter your name: ")
+print(a)
+#integer data
+b=int(input( "enter your phone_no: "))
+print(b)
+#input operation with if ,elif statement
+Age=int(input("enter your age: "))
+if Age <= 20:
+    print("you are not eligible for Graduation")
+else:
+    print("you are eligible for Graduation:", (Age))
+
